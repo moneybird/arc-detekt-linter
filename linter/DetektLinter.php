583b1b849507c4176b0bdbccb268833b5eee995f
Robbin Voortman <robbin@moneybird.com>
2018-01-24 14:45:44 +0100

    Update DetektLinter.php

--- linter/DetektLinter.php
+++ linter/DetektLinter.php
@@ -1,7 +1,24 @@
 <?php
 
-// source: https://secure.phabricator.com/D10405
-// author: josh-newman
+/*
+ * Copyright (C) 2018 Moneybird
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Robbin Voortman (robbin@moneybird.com)
+ * Date: 24-01-2018
+ */
+
 final class DetektLinter extends ArcanistExternalLinter {
 
   private $jarPath = null;
